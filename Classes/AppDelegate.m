a74e4887a3281c66f226007d64891bac10a021e9
Vishal Verma <vishal@zendrive.com>
2014-11-25 15:33:33 +0530

    Updating the project with the new 3.0.0 iOS SDK pod.

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -20,7 +20,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    self.window = [[UIWindow alloc] init];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[ViewController alloc] init];
 
     [self.window makeKeyAndVisible];
